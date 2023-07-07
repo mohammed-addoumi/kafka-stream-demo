d77db4eff1fb2099d4ea6191e64ea44d666fe3fc
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-07-07 12:52:15 +0100

    add kafka streams config

--- src/main/java/com/kafkastream/demo/configuration/KafkaStreamConfiguration.java
+++ src/main/java/com/kafkastream/demo/configuration/KafkaStreamConfiguration.java
@@ -1,16 +1,26 @@
 package com.kafkastream.demo.configuration;
 
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.kafkastreams.events.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @Configuration
 public class KafkaStreamConfiguration {
@@ -18,11 +28,13 @@ public class KafkaStreamConfiguration {
     @Bean
     public KafkaStreamsConfiguration kStreamsConfig() {
         Map<String, Object> props = new HashMap<>();
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, GenericAvroSerde.class);
+        props.put("schema.registry.url","http://localhost:8081");
+      //  props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.APPLICATION_ID_CONFIG,"kafka-streams-demo");
-        // Add other Kafka Streams configurations if needed
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG , "earliest");
         return new KafkaStreamsConfiguration(props);
     }
 
@@ -32,11 +44,23 @@ public class KafkaStreamConfiguration {
     }
 
     @Bean
-    public KStream<String, String> kStream(StreamsBuilder kStreamBuilder) {
-        KStream<String, String> inputStream = kStreamBuilder.stream("test_topic");
-        KStream<String, String> outputStream = inputStream.filter((k,v) -> v.contains("pass"))
-                                                            .mapValues(value -> value.toUpperCase());
+    public KStream<String, User> kStream(StreamsBuilder kStreamBuilder) {
+        final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url",
+                "http://localhost:8081");
+        final Serde<User> valueSpecificAvroSerde = new SpecificAvroSerde<>();
+        valueSpecificAvroSerde.configure(serdeConfig, false);
+        KStream<String, User> inputStream = kStreamBuilder
+                                            .stream("test_topic",
+                                                    Consumed.with(Serdes.String(),valueSpecificAvroSerde));
+
+        KStream<String, User> outputStream = inputStream.filter((k,v) -> v.getName().contains("pass"));
         outputStream.to("out_topic");
+
+        Topology build = kStreamBuilder.build();
+
+        KafkaStreams kafkaStreams = new KafkaStreams(build,kStreamsConfig().asProperties());
+        kafkaStreams.start();
+
         return outputStream;
     }
 }
